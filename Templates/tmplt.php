9d58bd61511e33651d2e026ff221e7e9111423f2
RndmSpn <122525945+RndmSpn@users.noreply.github.com>
2023-02-11 10:38:37 +0100

    Add templates via upload

---
+++ Templates/tmplt.php
@@ -0,0 +1,35 @@
+<html>
+
+<head></head>
+
+
+<header><header>
+
+<body>
+
+   <div >
+
+      <h1>PHP</h1>
+        
+        
+        <p>
+      
+          <?php
+         
+               
+             function dame_datos(){
+                 
+                echo "Mensaje Include";
+            
+            }
+         
+         ?>
+         
+        </p>
+    
+    <div>
+
+
+</body>
+
+</html>
